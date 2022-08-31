da223f234225ca4130c375145518fdea6d1bc49e
HodeBaiter <68841223+HodeBaiter@users.noreply.github.com>
2022-09-01 00:03:20 +0800

    kanban done

---
+++ jsjyz/src/main/resources/sql/keyWords.sql
@@ -0,0 +1,7 @@
+DROP TABLE IF EXISTS `key_words`;
+CREATE TABLE key_words
+(
+    id bigint(20) not null auto_increment primary key,
+    key_words varchar(255) not null,
+    article_id bigint(20) not null
+)
